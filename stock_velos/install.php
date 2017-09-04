a954fb53999ac05615baab9bf32a262091d12ea5
bohwaz <>
2017-09-04 23:58:27 +0000

    Le nom change : la migration se passe dans install, pas dans upgrade
    FossilOrigin-Name: 75f362f95a0f4f5aa29d1f3069a146c727dbe50d

--- stock_velos/install.php
+++ stock_velos/install.php
@@ -4,9 +4,17 @@ namespace Garradin;
 
 $db = DB::getInstance();
 
+// Renommage du plugin
+if ($db->test('plugins', $db->where('id', 'rustine_velos')))
+{
+    $db->delete('plugins_signaux', 'plugin = ?', 'rustine_velos');
+    $db->delete('plugins', 'id = ?', 'rustine_velos');
+    $db->exec('ALTER TABLE plugin_rustine_velos RENAME TO plugin_stock_velos;');
+}
+
 // Création table
 $db->exec(<<<EOF
-    CREATE TABLE plugin_stock_velos
+    CREATE TABLE IF NOT EXISTS plugin_stock_velos
     (
         id INTEGER NOT NULL PRIMARY KEY, -- Numéro unique
         etiquette INTEGER, -- Numéro étiquette
@@ -34,13 +42,13 @@ $db->exec(<<<EOF
         notes TEXT
     );
 
-    CREATE INDEX prv_etiquette ON plugin_stock_velos (etiquette);
-    CREATE INDEX prv_type ON plugin_stock_velos (type);
-    CREATE INDEX prv_roues ON plugin_stock_velos (roues);
-    CREATE INDEX prv_genre ON plugin_stock_velos (genre);
-    CREATE INDEX prv_couleur ON plugin_stock_velos (couleur);
-    CREATE INDEX prv_date_entree ON plugin_stock_velos (date_entree);
-    CREATE INDEX prv_date_sortie ON plugin_stock_velos (date_sortie);
+    CREATE INDEX IF NOT EXISTS prv_etiquette ON plugin_stock_velos (etiquette);
+    CREATE INDEX IF NOT EXISTS prv_type ON plugin_stock_velos (type);
+    CREATE INDEX IF NOT EXISTS prv_roues ON plugin_stock_velos (roues);
+    CREATE INDEX IF NOT EXISTS prv_genre ON plugin_stock_velos (genre);
+    CREATE INDEX IF NOT EXISTS prv_couleur ON plugin_stock_velos (couleur);
+    CREATE INDEX IF NOT EXISTS prv_date_entree ON plugin_stock_velos (date_entree);
+    CREATE INDEX IF NOT EXISTS prv_date_sortie ON plugin_stock_velos (date_sortie);
 EOF
 );
 
--- stock_velos/upgrade.php
+++ stock_velos/upgrade.php
@@ -5,9 +5,3 @@ namespace Garradin;
 $plugin->registerSignal('boucle.velos', ['Garradin\Velos_Signaux', 'LoopVelos']);
 
 $db = DB::getInstance();
-
-// Renomme table
-if ($db->test('sqlite_master' 'type = \'table\' AND name = \'plugin_rustine_velos\''))
-{
-	$db->exec('ALTER TABLE plugin_rustine_velos RENAME TO plugin_stock_velos;');
-}
