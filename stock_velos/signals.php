2ed065d7f587be91c87281c418df301f76501195
bohwaz <>
2017-10-17 03:33:50 +0000

    Correction nom de la table
    FossilOrigin-Name: c7e06daf6541ac668b31cd8b7f6d4c497100fb02

--- stock_velos/signals.php
+++ stock_velos/signals.php
@@ -10,12 +10,12 @@ class Velos_Signaux
 		{
 			if ($criteria['action'] == \KD2\MiniSkel::ACTION_MATCH_FIELD && $criteria['field'] == 'compter')
 			{
-				$return['query'] = 'SELECT COUNT(*) AS nb_en_vente FROM plugin_rustine_velos WHERE prix > 0 AND date_sortie IS NULL;';
+				$return['query'] = 'SELECT COUNT(*) AS nb_en_vente FROM plugin_stock_velos WHERE prix > 0 AND date_sortie IS NULL;';
 				break;
 			}
 			elseif ($criteria['action'] == \KD2\MiniSkel::ACTION_MATCH_FIELD && $criteria['field'] == 'liste')
 			{
-				$return['query'] = 'SELECT prix, modele, roues, type, genre, etiquette FROM plugin_rustine_velos
+				$return['query'] = 'SELECT prix, modele, roues, type, genre, etiquette FROM plugin_stock_velos
 					WHERE date_sortie IS NULL AND prix > 0 ORDER BY date_entree ASC;';
 				break;
 			}
