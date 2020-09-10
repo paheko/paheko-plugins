2bede712cdbb587a95cd2ee423cf4983c9c00721
bohwaz <>
2020-09-10 09:12:02 +0000

    Fix name of closing user
    FossilOrigin-Name: e7b7bedfba7252eb932e4b3740afa3a4ce452a43

--- caisse/lib/Session.php
+++ caisse/lib/Session.php
@@ -62,7 +62,7 @@ class Session
 		$sql = sprintf('SELECT x.a AS open_user_name, y.b AS close_user_name FROM
 			(SELECT %s AS a FROM membres WHERE id = ?) AS x,
 			(SELECT %1$s AS b FROM membres WHERE id = ?) AS y;', $db->quoteIdentifier($name_field));
-		return $db->firstColumn(POS::sql($sql), $this->open_user, $this->close_user);
+		return $db->first(POS::sql($sql), $this->open_user, $this->close_user);
 	}
 
 	public function hasOpenNotes() {
