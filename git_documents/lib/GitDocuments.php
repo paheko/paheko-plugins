6003a53ff191f9309e113503b2221113f120c1f0
bohwaz <>
2021-11-27 00:27:59 +0000

    To be able to run in background, this needs to be redirected to null
    FossilOrigin-Name: 1cafa565e63415fad859d013fccfc7c161ea153f

--- git_documents/lib/GitDocuments.php
+++ git_documents/lib/GitDocuments.php
@@ -21,13 +21,13 @@ class GitDocuments
 			$user_arg = '';
 		}
 
-		$cmd = '{cd %s && git reset --merge 2>&1 && git add -A 2>&1 && git commit -a -m %s %s 2>&1} &';
+		$cmd = '{ cd %s && git reset --merge 2>&1 && git add -A 2>&1 && git commit -a -m %s %s 2>&1; } > /dev/null &';
 		$cmd = sprintf($cmd, escapeshellarg(\Garradin\FILE_STORAGE_CONFIG), escapeshellarg(self::MESSAGE), $user_arg);
 
 		exec($cmd, $out, $code);
 
 		if ($code) {
-			//throw new \RuntimeException(sprintf('Git command (%s) failed: %s', $cmd, implode("\n", $out)));
+			throw new \RuntimeException(sprintf('Git command (%s) failed: %s', $cmd, implode("\n", $out)));
 		}
 	}
 }
