8f4e7a5fef05202b13fbac938080ad5bb54da2d8
bohwaz <>
2022-01-03 17:17:39 +0000

    Fix old entries that were using the wrong week number
    FossilOrigin-Name: dbf98ac0f7e12832ad442044f51ee3d523d95b42

---
+++ taima/upgrade.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Garradin;
+
+$old_version = $plugin->getInfos('version');
+$db = DB::getInstance();
+
+// Fix year for dates that are in the last week of previous year
+// eg. 2022-01-01 is in week 52 of year 2021
+if (version_compare($old_version, '0.4', '<')) {
+	$dates = ['2022-01-01', '2022-01-02', '2021-01-01', '2021-01-02', '2021-01-03'];
+	$db->exec(sprintf('UPDATE plugin_taima_entries SET year = year - 1 WHERE %s;', $db->where('date', $dates)));
+}
