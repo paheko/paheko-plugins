5399be1f3cc6edb75142faf19f9bc914ccfb7175
bohwaz <>
2022-06-01 21:26:03 +0000

    Change signal
    FossilOrigin-Name: 7f26943019ff1ae9bfa842f6485976c817c52135

--- welcome/install.php
+++ welcome/install.php
@@ -5,4 +5,4 @@ namespace Garradin\Plugin;
 use Garradin\Plugin\Welcome\Signaux;
 
 // enregistrer signal pour message page d'accueil
-$plugin->registerSignal('accueil.banniere', [Signaux::class, 'banner']);
+$plugin->registerSignal('home.banner', [Signaux::class, 'banner']);
