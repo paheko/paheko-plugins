2f8da1657df4ea2b60fab6414570b60da983bd25
bohwaz <>
2022-06-30 16:53:36 +0000

    Restrict access to statistics
    FossilOrigin-Name: b8b3aa845e69e95cc77127419867fd071cca69dc

--- taima/www/admin/stats.php
+++ taima/www/admin/stats.php
@@ -8,6 +8,8 @@ use function Garradin\{f, qg};
 
 require_once __DIR__ . '/_inc.php';
 
+$session->requireAccess($session::SECTION_USERS, $session::ACCESS_ADMIN);
+
 $per_user = qg('per_user') !== null;
 $grouping = qg('g') ?? 'week';
 $per_week = Tracking::listPerInterval($grouping, $per_user);
