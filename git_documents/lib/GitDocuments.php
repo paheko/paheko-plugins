632bc1bac246cb3546d20ac29cb070b7d5481dbe
bohwaz <>
2022-10-22 20:48:01 +0000

    Add diff to git_documents
    FossilOrigin-Name: d673cb75cdb5b7c0dcf2e586a1e9287c5d7028e9

--- git_documents/lib/GitDocuments.php
+++ git_documents/lib/GitDocuments.php
@@ -4,6 +4,11 @@ namespace Garradin\Plugin\Git_Documents;
 
 use Garradin\Membres\Session;
 
+use Garradin\Plugin;
+use Garradin\Users\Emails;
+
+use KD2\Mail_Message;
+
 class GitDocuments
 {
 	const MESSAGE = "Modification depuis l'interface de Garradin";
@@ -21,7 +26,7 @@ class GitDocuments
 			$user_arg = '';
 		}
 
-		$cmd = '{ cd %s && git reset --merge 2>&1 && git add -A 2>&1 && git commit -a -m %s %s 2>&1; } > /dev/null &';
+		$cmd = '{ cd %s && git reset --merge 2>&1 && git add -A 2>&1 && git commit -a -m %s %s 2>&1; } > /dev/null';
 		$cmd = sprintf($cmd, escapeshellarg(\Garradin\FILE_STORAGE_CONFIG), escapeshellarg(self::MESSAGE), $user_arg);
 
 		putenv('LANG=fr_FR.UTF-8');
@@ -30,5 +35,60 @@ class GitDocuments
 		if ($code) {
 			throw new \RuntimeException(sprintf('Git command (%s) failed: %s', $cmd, implode("\n", $out)));
 		}
+
+		self::sendDiff();
+	}
+
+	static public function sendDiff(): void
+	{
+		$plugin = new Plugin('git_documents');
+		$config = $plugin->getConfig();
+
+		if (empty($config->diff_email)) {
+			return;
+		}
+
+		$body = '';
+
+		if (!empty($config->last_commit_hash)) {
+			$revs = sprintf('HEAD...%s', $config->last_commit_hash);
+		}
+		else {
+			$revs = 'HEAD...HEAD^';
+		}
+
+		$diff_cmd = sprintf('cd %s && git log -p %s',
+			escapeshellarg(\Garradin\FILE_STORAGE_CONFIG),
+			escapeshellarg($revs)
+		);
+
+		$body .= shell_exec($diff_cmd . ' --no-color');
+
+		$html = null;
+
+		if (shell_exec('which aha')) {
+			$html = shell_exec($diff_cmd . ' --color | aha');
+		}
+
+		$msg = new Mail_Message;
+		$msg->setHeader('To', $config->diff_email);
+		$msg->setHeader('From', Emails::getFromHeader());
+		$msg->setHeader('Subject', 'Modification de documents');
+		$msg->setBody($body);
+
+		if ($html) {
+			$msg->addPart('text/html', $html);
+		}
+
+		Emails::sendMessage(Emails::CONTEXT_SYSTEM, $msg);
+
+		/*
+		$date = shell_exec(sprintf('cd %s && git log -n 1 | sed -n -e "3,3p"', escapeshellarg(\Garradin\FILE_STORAGE_CONFIG)));
+		$date = str_replace('Date:', '', $date);
+		$date = trim($date);
+		$date = strtotime($date);
+		*/
+		$last = trim(shell_exec(sprintf('cd %s && git rev-parse HEAD', escapeshellarg(\Garradin\FILE_STORAGE_CONFIG))));
+		$plugin->setConfig('last_commit_hash', $last);
 	}
 }
