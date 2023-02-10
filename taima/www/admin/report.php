5b1b212aac76cf3dd4d71bf338ca60a065e6b642
bohwaz <>
2023-02-10 21:08:04 +0000

    Taima: Ask for which year reporting should be done, if multiple open years exist
    FossilOrigin-Name: 6ab0485f6bc8f7fd97349c7b01d9f342f1b1d117

--- taima/www/admin/report.php
+++ taima/www/admin/report.php
@@ -16,24 +16,38 @@ $session->requireAccess($session::SECTION_USERS, $session::ACCESS_ADMIN);
 $session->requireAccess($session::SECTION_ACCOUNTING, $session::ACCESS_WRITE);
 
 $csrf_key = 'taima_report';
-$year = Years::getCurrentOpenYear();
+$years = Years::listOpenAssoc();
 
-if (!$year) {
+if (!count($years)) {
 	throw new UserException('Aucun exercice n\'est ouvert, il n\'est donc pas possible de valoriser le temps bénévole.');
 }
 
-$start = f('start') ? Utils::get_datetime(f('start')) : $year->start_date;
-$end = f('end') ? Utils::get_datetime(f('end')) : $year->end_date;
+$year = null;
 
-$form->runIf('save', function () use ($year, $start, $end) {
-	$id_user = Session::getInstance()->getUser()->id;
-	$t = Tracking::createReport($year, $start, $end, $id_user);
-	$t->save();
-	Utils::redirect(Utils::getSelfURI(['ok' => $t->id()]));
-}, $csrf_key);
+if (f('id_year')) {
+	$year = Years::get((int)f('id_year'));
+}
+elseif (count($years) == 1) {
+	$year = Years::getCurrentOpenYear();
+}
 
-$report = Tracking::getFinancialReport($year, $start, $end);
+if ($year) {
+	$start = f('start') ? Utils::get_datetime(f('start')) : ($year->start_date ?? null);
+	$end = f('end') ? Utils::get_datetime(f('end')) : ($year->end_date ?? null);
 
-$tpl->assign(compact('report', 'year', 'csrf_key'));
+	$form->runIf('save', function () use ($year, $start, $end) {
+		$id_user = Session::getInstance()->getUser()->id;
+		$t = Tracking::createReport($year, $start, $end, $id_user);
+		$t->save();
+		Utils::redirect(Utils::getSelfURI(['ok' => $t->id()]));
+	}, $csrf_key);
+
+	$report = Tracking::getFinancialReport($year, $start, $end);
+
+	$tpl->assign(compact('report', 'year', 'csrf_key'));
+}
+else {
+	$tpl->assign(compact('years', 'csrf_key'));
+}
 
 $tpl->display(__DIR__ . '/../../templates/report.tpl');
