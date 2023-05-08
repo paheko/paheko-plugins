35d5680052902298624f43813f1a9c9f11419e43
alinaar <alinaar@noemail.net>
2023-05-08 10:05:11 +0000

    HelloAsso: "targets" controller fixed
    FossilOrigin-Name: 140f8def053be6791495b1bc9b44142c122cc4a8

--- helloasso/admin/targets.php
+++ helloasso/admin/targets.php
@@ -3,12 +3,11 @@
 namespace Garradin;
 
 use Garradin\Plugin\HelloAsso\HelloAsso;
+use Garradin\Plugin\HelloAsso\Forms;
 
 $session->requireAccess($session::SECTION_USERS, $session::ACCESS_ADMIN);
 
-$ha = HelloAsso::getInstance();
-
-$list = $ha->listForms();
+$list = Forms::list();
 $tpl->assign('list', $list);
 
 $tpl->display(PLUGIN_ROOT . '/templates/index.tpl');
