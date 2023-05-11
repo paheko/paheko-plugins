58d7503f1c752c92d13f62f10e2022e8c8134e7f
bohwaz <>
2023-05-11 14:16:11 +0000

    webstats: Fix DB object
    FossilOrigin-Name: 7b196be7f486504db8ff805a4bcbb19f5f3d2787

--- webstats/install.php
+++ webstats/install.php
@@ -1,7 +1,10 @@
 <?php
 
+namespace Garradin;
+
 use Garradin\Plugin\Webstats\Stats;
 
+$db = DB::getInstance();
 $db->import(__DIR__ . '/schema.sql');
 
 $plugin->registerSignal('web.request', 'Garradin\Plugin\Webstats\Stats::webRequest');
