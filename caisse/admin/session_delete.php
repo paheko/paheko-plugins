a1afe01eb7b8f70d408d126ba2fa695004c9f5f3
bohwaz <>
2023-06-13 14:36:40 +0000

    Caisse: implement delete of session
    FossilOrigin-Name: ff340ddddbbe7b051503a9a93aec8b6ab3e57e5b

---
+++ caisse/admin/session_delete.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Garradin;
+
+use Garradin\Users\Session;
+use Garradin\Plugin\Caisse\Sessions;
+use function Garradin\Plugin\Caisse\get_amount;
+
+require __DIR__ . '/_inc.php';
+
+Session::getInstance()->requireAccess(Session::SECTION_ACCOUNTING, Session::ACCESS_ADMIN);
+
+$pos_session = Sessions::get((int)qg('id'));
+
+if (!$pos_session) {
+	throw new UserException('Numéro de session inconnu');
+}
+
+if (!$pos_session->closed) {
+	throw new UserException('Cette session n\'est pas clôturée');
+}
+
+$csrf_key = 'pos_delete_' . $pos_session->id();
+
+$form->runIf(f('delete') && f('confirm_delete'), fn() => $pos_session->delete(), $csrf_key, Utils::plugin_url());
+
+$tpl->assign(compact('csrf_key', 'pos_session'));
+
+$tpl->display(PLUGIN_ROOT . '/templates/session_delete.tpl');
