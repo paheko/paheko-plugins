bf6cd1ee64e4bd13535d166e556f05fb5a93a933
bohwaz <>
2023-08-21 23:09:05 +0000

    Ignore mkdir now
    FossilOrigin-Name: 19ae4ff1b2545587a2ca774649cd764c2eda8229

--- git_documents/install.php
+++ git_documents/install.php
@@ -5,4 +5,3 @@ namespace Paheko;
 $plugin->registerSignal('files.move', 'Paheko\Plugin\Git_Documents\GitDocuments::sync');
 $plugin->registerSignal('files.delete', 'Paheko\Plugin\Git_Documents\GitDocuments::sync');
 $plugin->registerSignal('files.store', 'Paheko\Plugin\Git_Documents\GitDocuments::sync');
-$plugin->registerSignal('files.mkdir', 'Paheko\Plugin\Git_Documents\GitDocuments::sync');
