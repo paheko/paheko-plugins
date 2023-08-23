217edc29e20d08928dce280e5cf1ee70bd160304
bohwaz <>
2023-08-23 12:05:44 +0000

    Remove email notifications from GitDocuments
    FossilOrigin-Name: f4ae33452b65769dacd6057213017ef21f044dfe

--- git_documents/lib/GitDocuments.php
+++ git_documents/lib/GitDocuments.php
@@ -5,9 +5,6 @@ namespace Paheko\Plugin\Git_Documents;
 use Paheko\Users\Session;
 
 use Paheko\Plugin;
-use Paheko\Users\Emails;
-
-use KD2\Mail_Message;
 
 class GitDocuments
 {
@@ -35,61 +32,5 @@ class GitDocuments
 		if ($code) {
 			throw new \RuntimeException(sprintf('Git command (%s) failed: %s', $cmd, implode("\n", $out)));
 		}
-	}
-
-	static public function sendDiff(): void
-	{
-		$plugin = new Plugin('git_documents');
-		$config = $plugin->getConfig();
-
-		if (empty($config->diff_email)) {
-			return;
-		}
-
-		$body = '';
-		$last = trim(shell_exec(sprintf('cd %s && git rev-parse HEAD', escapeshellarg(\Paheko\FILE_STORAGE_CONFIG))));
-
-		if (!empty($config->last_commit_hash)) {
-			if ($last == $config->last_commit_hash) {
-				return;
-			}
-
-			$revs = sprintf('HEAD...%s', $config->last_commit_hash);
-		}
-		else {
-			$revs = 'HEAD...HEAD^';
-		}
-
-		$plugin->setConfig('last_commit_hash', $last);
-
-		putenv('LANG=fr_FR.UTF-8');
-		$diff_cmd = sprintf('cd %s && git log -p %s',
-			escapeshellarg(\Paheko\FILE_STORAGE_CONFIG),
-			escapeshellarg($revs)
-		);
-
-		$body = shell_exec($diff_cmd . ' --no-color');
-
-		if (!trim($body)) {
-			return;
-		}
-
-		$html = null;
-
-		if (shell_exec('which aha')) {
-			$html = shell_exec($diff_cmd . ' --color --word-diff=color | aha');
-		}
-
-		$msg = new Mail_Message;
-		$msg->setHeader('To', $config->diff_email);
-		$msg->setHeader('From', Emails::getFromHeader());
-		$msg->setHeader('Subject', 'Modification de documents');
-		$msg->setBody($body);
-
-		if ($html) {
-			$msg->addPart('text/html', $html);
-		}
-
-		Emails::sendMessage(Emails::CONTEXT_SYSTEM, $msg);
 	}
 }
--- git_documents/public/send_diff.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-Paheko\Plugin\Git_Documents\GitDocuments::sendDiff();
-
-http_response_code(204);
-exit;
