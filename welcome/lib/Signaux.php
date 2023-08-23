c5b5092c7aa301f046b25aa5e6ddda6f20d37a53
bohwaz <>
2023-08-23 12:30:44 +0000

    Fix typo
    FossilOrigin-Name: cbccef579d38fd3adb1fd03a9fe4d56b6f79d8a7

--- welcome/lib/Signaux.php
+++ welcome/lib/Signaux.php
@@ -9,7 +9,7 @@ use Paheko\Template;
 
 class Signaux
 {
-	static public function banner(Signnal $signal)
+	static public function banner(Signal $signal)
 	{
 		$html = Template::getInstance()->fetch(__DIR__ . '/../templates/banner.tpl');
 		$signal->setOut('welcome', $html);
