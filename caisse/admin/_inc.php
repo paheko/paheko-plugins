3a40560fcb4d164ad0098be707e49eb1a65fb3bb
bohwaz <>
2023-12-16 20:00:33 +0000

    Remove image_base64 modifier
    FossilOrigin-Name: 7c02580051088f784308465f2a22dd1d980ddff5

--- caisse/admin/_inc.php
+++ caisse/admin/_inc.php
@@ -14,10 +14,6 @@ function get_amount(string $amount): int {
 	return Utils::moneyToInteger($amount);
 }
 
-$tpl->register_modifier('image_base64', function (string $blob) {
-	return 'data:image/png;base64,' . base64_encode($blob);
-});
-
 Session::getInstance()->requireAccess(Session::SECTION_USERS, Session::ACCESS_WRITE);
 
 $tpl->assign('pos_templates_root', \Paheko\PLUGIN_ROOT . '/templates');
