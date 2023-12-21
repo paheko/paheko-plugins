6580be7e47ca31f58be9e6d84d14674e18ca7bec
bohwaz <>
2023-12-21 17:38:49 +0000

    Fix example data when setting up caisse
    FossilOrigin-Name: cd852b1efd71c81155337a2668a46e2d04e3183d

--- caisse/data.sql
+++ caisse/data.sql
@@ -1,17 +1,17 @@
-INSERT INTO @PREFIX_categories VALUES (1, "Adhésion", NULL);
-INSERT INTO @PREFIX_products VALUES (NULL, 1, "Adhésion normale", NULL, 1500, 1, NULL, NULL);
-INSERT INTO @PREFIX_products VALUES (NULL, 1, "Adhésion réduite", NULL, 1000, 1, NULL, NULL);
+INSERT INTO @PREFIX_categories (id, name) VALUES (1, "Adhésion");
+INSERT INTO @PREFIX_products (category, name, price) VALUES (1, "Adhésion normale", 1500);
+INSERT INTO @PREFIX_products (category, name, price) VALUES (1, "Adhésion réduite", 1000);
 
-INSERT INTO @PREFIX_categories VALUES (4, "Pièces de vélo d'occasion", NULL);
+INSERT INTO @PREFIX_categories (id, name) VALUES (4, "Pièces de vélo d'occasion");
 INSERT INTO @PREFIX_products (category, name, price) VALUES
 	(4, "Dérailleur (avant, arrière)", 300),
 	(4, "Panier", 500);
 
-INSERT INTO @PREFIX_methods VALUES (1, 'Espèces', 1, NULL, NULL, '530', 1);
-INSERT INTO @PREFIX_methods VALUES (2, 'Chèque', 0, NULL, NULL, '5112', 1);
+INSERT INTO @PREFIX_methods (id, name, is_cash, account) VALUES (1, 'Espèces', 1, '530');
+INSERT INTO @PREFIX_methods (id, name, is_cash, account) VALUES (2, 'Chèque', 0, '5112');
 
 -- Ajout espèces/chèque
-INSERT INTO @PREFIX_products_methods SELECT id, 1 FROM @PREFIX_products;
-INSERT INTO @PREFIX_products_methods SELECT id, 2 FROM @PREFIX_products;
+INSERT INTO @PREFIX_products_methods (product, method) SELECT id, 1 FROM @PREFIX_products;
+INSERT INTO @PREFIX_products_methods (product, method) SELECT id, 2 FROM @PREFIX_products;
 
 UPDATE @PREFIX_products SET stock = 0 WHERE category IN (3, 4, 5);
