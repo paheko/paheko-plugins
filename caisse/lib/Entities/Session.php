4e6c39950da7f5da02af95ee39a55549f9c076b7
bohwaz <>
2025-01-29 23:03:56 +0000

    Return a better error message if the subscription already exists for the user
    FossilOrigin-Name: 8b926b26368384523ab0176d70514c9890cb064e

--- caisse/lib/Entities/Session.php
+++ caisse/lib/Entities/Session.php
@@ -124,15 +124,20 @@ class Session extends Entity
 		$db->preparedQuery(POS::sql(sprintf('UPDATE @PREFIX_products SET stock = -(SELECT SUM(ti.qty) %s) + stock WHERE stock IS NOT NULL AND id IN (SELECT DISTINCT ti.product %1$s);', $select)));
 
 		// Create subscriptions
-		$sql = POS::sql('SELECT ti.id, t.user_id, ti.id_fee, ti.total, ti.qty
+		$sql = POS::sql('SELECT ti.tab, ti.id, t.user_id, ti.id_fee, ti.total, ti.qty
 			FROM @PREFIX_tabs_items ti
 			INNER JOIN @PREFIX_tabs t ON t.id = ti.tab
 			WHERE ti.id_fee IS NOT NULL AND session = ?;');
 
 		foreach ($db->iterate($sql, $this->id) as $row) {
-			$su = Services_User::createFromFee($row->id_fee, $row->user_id, $row->total, true, $row->qty);
-			$su->save();
-			$db->update(TabItem::TABLE, ['id_subscription' => $su->id()], 'id = ' . (int)$row->id);
+			try {
+				$su = Services_User::createFromFee($row->id_fee, $row->user_id, $row->total, true, $row->qty);
+				$su->save();
+				$db->update(TabItem::TABLE, ['id_subscription' => $su->id()], 'id = ' . (int)$row->id);
+			}
+			catch (UserException $e) {
+				throw new UserException(sprintf('Note n°%d : %s', $row->tab, $e->getMessage()));
+			}
 		}
 
 		$db->commit();
