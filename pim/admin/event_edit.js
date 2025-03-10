75d391811e004223e84db159961f51e0d1142f95
bohwaz <>
2025-03-10 10:35:57 +0000

    Improve handling of date fields when editing an event
    FossilOrigin-Name: 04939d3a910f325b954d1973473f352562266b06

--- pim/admin/event_edit.js
+++ pim/admin/event_edit.js
@@ -2,6 +2,8 @@ var t = $('#f_title');
 var start_time = $('#f_start_time');
 var end_time = $('#f_end_time');
 var all_day = $('#f_all_day_1');
+var start_day = $('#f_start');
+var end_day = $('#f_end');
 var loc = $('#f_location');
 var id_category = $('#f_id_category');
 
@@ -29,6 +31,16 @@ id_category.onchange = selectCategory;
 
 selectCategory();
 
+function dateToInteger(str) {
+	var r = str.match(/^(\d+)\/(\d+)\/(\d+)$/);
+
+	if (!r) {
+		return null;
+	}
+
+	return parseInt(r[3] + r[2] + r[1], 10);
+}
+
 function formatTime(h, m) {
 	h = ('00' + h).substr(-2);
 	m = ('00' + m).substr(-2);
@@ -44,12 +56,15 @@ function formatTime(h, m) {
 	return h + ':' + m;
 }
 
-all_day.onchange = () => {
+function changeAllDay() {
 	if (all_day.checked) {
 		start_time.value = '00:00';
 		end_time.value = '00:00';
 	}
-};
+
+	start_time.disabled = all_day.checked;
+	end_time.disabled = all_day.checked;
+}
 
 function changeTime() {
 	if (start_time.value === '00:00'
@@ -59,10 +74,26 @@ function changeTime() {
 	else {
 		all_day.checked = false;
 	}
+
+	changeAllDay();
+}
+
+function changeStartDay() {
+	var s = dateToInteger(start_day.value);
+	var e = dateToInteger(end_day.value);
+
+	if (s && s > e) {
+		end_day.value = start_day.value;
+		animateInput(end_day);
+	}
 }
 
 start_time.onchange = changeTime;
 end_time.onchange = changeTime;
+all_day.onchange = changeAllDay;
+start_day.oninput = changeStartDay;
+
+changeTime();
 
 function ciEquals(a, b) {
 	return typeof a === 'string' && typeof b === 'string'
