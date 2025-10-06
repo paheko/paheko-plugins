dd3535ba7abf5c78f93248fd770128f01e5e0788
bohwaz <>
2025-10-06 17:44:12 +0000

    DROP COLUMN is only available in SQLite 3.35.0+, don't use it
    FossilOrigin-Name: f5547ae8b2e55c4003d2fdaaf0f6494f9ba7d133

--- caisse/update_0.8.10.sql
+++ caisse/update_0.8.10.sql
@@ -3,4 +3,20 @@ ALTER TABLE @PREFIX_methods ADD COLUMN is_default INTEGER NOT NULL DEFAULT 0;
 
 UPDATE @PREFIX_methods SET is_default = 1 WHERE position = 1 LIMIT 1;
 
-ALTER TABLE @PREFIX_methods DROP COLUMN position;
+ALTER TABLE @PREFIX_methods RENAME TO @PREFIX_methods_old;
+
+CREATE TABLE IF NOT EXISTS @PREFIX_methods (
+	-- Payment methods
+	id INTEGER NOT NULL PRIMARY KEY,
+	id_location INTEGER NULL REFERENCES @PREFIX_locations (id) ON DELETE CASCADE,
+	name TEXT NOT NULL,
+	type INTEGER NOT NULL DEFAULT 0,
+	min INTEGER NULL, -- Minimum amount that can be paid using this method
+	max INTEGER NULL, -- Maximum amount that can be paid using this method
+	account TEXT NULL, -- Accounting account code
+	is_default INTEGER NOT NULL DEFAULT 0,
+	enabled INTEGER NOT NULL DEFAULT 1
+);
+
+INSERT INTO @PREFIX_methods SELECT id, id_location, name, type, min, max, account, is_default, enabled FROM @PREFIX_methods_old;
+DROP TABLE @PREFIX_methods_old;
