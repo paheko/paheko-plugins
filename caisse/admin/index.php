fb8a89002f69672efe4e930ecde7313db560d6b5
bohwaz <>
2026-02-27 17:02:37 +0000

    Fix update
    FossilOrigin-Name: 035568b8f8e59b6d3f0ec35eb83437d1a5f2b03c

--- caisse/admin/index.php
+++ caisse/admin/index.php
@@ -11,8 +11,8 @@ if ($plugin->needUpgrade()) {
 }
 
 // @FIXME: temporary fix, if the update failed at some point
-if (!DB::getInstance()->hasTable('plugin_pos_sessions_balances')) {
-	$plugin->set('version', '0.8.11');
+if (!DB::getInstance()->hasTable('plugin_pos_products_links')) {
+	$plugin->set('version', '0.8.10');
 	$plugin->upgrade();
 }
 
