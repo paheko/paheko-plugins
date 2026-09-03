057f6ee06b6a6f4189067519f353cab1b991ca46
bohwaz <>
2026-09-03 18:27:05 +0000

    Taima: block other users from modifying any entry
    FossilOrigin-Name: a3868c890b8fe56efa4f9b8fe635cfc20296bfbc

--- taima/admin/edit.php
+++ taima/admin/edit.php
@@ -5,11 +5,10 @@ namespace Paheko\Plugin\Taima;
 use Paheko\Plugin\Taima\Tracking;
 use Paheko\Plugin\Taima\Entities\Entry;
 use Paheko\Form;
+use Paheko\Users\Session;
 use Paheko\Users\Users;
 use Paheko\Utils;
 use Paheko\UserException;
-
-use function Paheko\{f, qg};
 
 use KD2\DB\Date;
 
@@ -17,11 +16,12 @@ require_once __DIR__ . '/_inc.php';
 
 $csrf_key = 'edit_task';
 $selected_user = null;
+$session = Session::getInstance();
 
-$user = qg('id_user');
-
-if ($user) {
-	$user = Users::get((int)$user);
+// Pre-fill user name
+if ($session->canAccess($session::SECTION_USERS, $session::ACCESS_WRITE)
+	&& isset($_GET['id_user'])) {
+	$user = Users::get((int) $_GET['id_user']);
 
 	if (!$user) {
 		throw new UserException('Membre inconnu');
@@ -30,22 +30,26 @@ if ($user) {
 	$selected_user = [$user->id => $user->name()];
 }
 
-if (qg('from')) {
-	$entry = Tracking::get((int)qg('from'));
+// Duplicate from ID
+if (isset($_GET['from'])) {
+	$entry = Tracking::get((int) $_GET['from']);
 	$entry = clone $entry;
 	$entry_duration = Tracking::formatMinutes($entry->duration);
 }
-elseif (qg('id')) {
-	$entry = Tracking::get((int)qg('id'));
+// Edit existing entry
+elseif (isset($_GET['id'])) {
+	$entry = Tracking::get((int) $_GET['id']);
 	$entry_duration = Tracking::formatMinutes($entry->duration);
-	$selected_user = $user ? [$user->id => $user->name()] : null;
 }
+// Create new entry
 else {
 	$entry = new Entry;
 	$entry_duration = null;
+	$entry->set('user_id', $session::getUserId());
 
+	// If date is set, it means it's for the current user
 	if (isset($_GET['date'])) {
-		$entry->importForm(['date' => $_GET['date'], 'user_id' => $session::getUserId()]);
+		$entry->importForm(['date' => $_GET['date']]);
 	}
 }
 
@@ -54,10 +58,26 @@ if (!$session->canAccess($session::SECTION_USERS, $session::ACCESS_WRITE)
 	throw new UserException('Vous n\'avez pas accès à cette tâche');
 }
 
-$form->runIf('save', function () use ($entry) {
-	$entry->importForm();
+if ($entry->exists() || isset($_GET['from'])) {
+	$selected_user ??= $entry->user_id ? [$entry->user_id => $entry->user_name()] : null;
+}
+
+$form->runIf('save', function () use ($entry, $session) {
+	$data = $_POST;
+
+	// Users who can only create entries cannot create entries other than for themselves
+	if (!$session->canAccess($session::SECTION_USERS, $session::ACCESS_WRITE)) {
+		unset($data['user_id'], $data['user']);
+	}
+	else {
+		$data['user'] ??= [];
+	}
+
+	$entry->importForm($data);
 	$entry->save();
-}, $csrf_key, Utils::getSelfURI(['ok' => 1]));
+
+	Utils::reloadParentFrameIfDialog();
+}, $csrf_key);
 
 $tasks = Tracking::listTasks();
 $now = new Date;
