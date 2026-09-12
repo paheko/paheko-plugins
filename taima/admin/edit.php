88c1672a7f4ece724e5ee97f9657b974a24d4e84
bohwaz <>
2026-09-12 11:41:56 +0000

    Allow to select multiple users for creating taima entries
    FossilOrigin-Name: bfc27eb0ba3807cd7b1a26c81c9a0e5e56878b9c

--- taima/admin/edit.php
+++ taima/admin/edit.php
@@ -4,79 +4,108 @@ namespace Paheko\Plugin\Taima;
 
 use Paheko\Plugin\Taima\Tracking;
 use Paheko\Plugin\Taima\Entities\Entry;
-use Paheko\Form;
 use Paheko\Users\Session;
 use Paheko\Users\Users;
 use Paheko\Utils;
 use Paheko\UserException;
 
 use KD2\DB\Date;
+use KD2\Form as Form;
 
 require_once __DIR__ . '/_inc.php';
 
 $csrf_key = 'edit_task';
-$selected_user = null;
+$selected_users = null;
 $session = Session::getInstance();
 
-// Pre-fill user name
-if ($session->canAccess($session::SECTION_USERS, $session::ACCESS_WRITE)
-	&& isset($_GET['id_user'])) {
-	$user = Users::get((int) $_GET['id_user']);
-
-	if (!$user) {
-		throw new UserException('Membre inconnu');
-	}
-
-	$selected_user = [$user->id => $user->name()];
-}
-
 // Duplicate from ID
-if (isset($_GET['from'])) {
-	$entry = Tracking::get((int) $_GET['from']);
+if ($id = Form::getQueryInt('copy')) {
+	$entry = Tracking::get($id);
 	$entry = clone $entry;
-	$entry_duration = Tracking::formatMinutes($entry->duration);
+
+	// If user doesn't have write access, reset user_id
+	if (!$session->canAccess($session::SECTION_USERS, $session::ACCESS_WRITE)) {
+		$entry->set('user_id', null);
+	}
 }
 // Edit existing entry
-elseif (isset($_GET['id'])) {
-	$entry = Tracking::get((int) $_GET['id']);
-	$entry_duration = Tracking::formatMinutes($entry->duration);
+elseif ($id = Form::getQueryInt('id')) {
+	$entry = Tracking::get($id);
 }
 // Create new entry
 else {
 	$entry = new Entry;
-	$entry_duration = null;
-	$entry->set('user_id', $session::getUserId());
 
 	// If date is set, it means it's for the current user
-	if (isset($_GET['date'])) {
-		$entry->importForm(['date' => $_GET['date']]);
+	if ($date = Form::getQueryString('date')) {
+		$entry->set('user_id', $session::getUserId());
+		$entry->importForm(compact('date'));
 	}
 }
 
-if (!$session->canAccess($session::SECTION_USERS, $session::ACCESS_WRITE)
-	&& (!$session->isLogged() || $entry->user_id !== $session::getUserId())) {
-	throw new UserException('Vous n\'avez pas accès à cette tâche');
+if ($entry->exists()) {
+	// users with write_access can edit entries for anyone
+	// others can only edit their own entries
+	if (!$session->canAccess($session::SECTION_USERS, $session::ACCESS_WRITE)
+		&& $entry->user_id !== $session::getUserId()) {
+		throw new UserException('Vous n\'avez pas accès à cette tâche');
+	}
+
+	$allow_multiple_users = false;
+}
+else {
+	if ($session->canAccess($session::SECTION_USERS, $session::ACCESS_WRITE)) {
+		$allow_multiple_users = true;
+
+		if ($id_user = Form::getQueryInt('id_user')) {
+			$user = Users::get($id_user);
+
+			if (!$user) {
+				throw new UserException('Membre inconnu');
+			}
+
+			$entry->set('user_id', $user->id);
+		}
+	}
+	else {
+		$entry->set('user_id', $session::getUserId());
+		$allow_multiple_users = false;
+	}
 }
 
-if ($entry->exists() || isset($_GET['from'])) {
-	$selected_user ??= $entry->user_id ? [$entry->user_id => $entry->user_name()] : null;
-}
+$selected_users = $entry->user_id ? [$entry->user_id => $entry->user_name()] : null;
+$entry_duration = isset($entry->duration) ? Tracking::formatMinutes($entry->duration) : null;
 
 $form->runIf('save', function () use ($entry, $session) {
 	$data = $_POST;
+	unset($data['user_id']);
+	$entry->importForm($data);
 
 	// Users who can only create entries cannot create entries other than for themselves
-	if (!$session->canAccess($session::SECTION_USERS, $session::ACCESS_WRITE)) {
-		unset($data['user_id'], $data['user']);
+	if (!$entry->exists()
+		&& $session->canAccess($session::SECTION_USERS, $session::ACCESS_WRITE)) {
+		$users = Form::getPostArray('users');
+
+		if (!$entry->user_id && count($users)) {
+			foreach ($users as $id => $name) {
+				$entry = clone $entry;
+				$entry->set('user_id', $id);
+				$entry->save();
+			}
+
+			Utils::redirect('!p/taima/all.php');
+		}
+		else {
+			// User is NULL
+			$entry->save();
+		}
 	}
-	elseif (!$entry->user_id) {
-		$data['user'] ??= [];
+	else {
+		// Existing entry
+		$entry->save();
 	}
 
-	$entry->importForm($data);
-	$entry->save();
-
-	Utils::reloadParentFrameIfDialog();
+	Utils::reloadParentFrameIfDialog('!p/taima/');
 }, $csrf_key);
 
 $tasks = Tracking::listTasks();
@@ -85,6 +114,6 @@ $date = isset($_GET['date']);
 $is_today = $date && $_GET['date'] === date('Y-m-d');
 $submit_label = $is_today && !$entry->duration ? 'Démarrer le chrono' : 'Enregistrer';
 
-$tpl->assign(compact('tasks', 'csrf_key', 'now', 'selected_user', 'entry', 'entry_duration', 'date', 'is_today', 'submit_label'));
+$tpl->assign(compact('tasks', 'csrf_key', 'now', 'selected_users', 'allow_multiple_users', 'entry', 'entry_duration', 'date', 'is_today', 'submit_label'));
 
 $tpl->display(\Paheko\PLUGIN_ROOT . '/templates/edit.tpl');
--- taima/templates/edit.tpl
+++ taima/templates/edit.tpl
@@ -19,9 +19,10 @@
 	<fieldset>
 		<legend>{$title}</legend>
 		<dl>
+			<?php $label = $allow_multiple_users ? 'Membres' : 'Membre'; ?>
 			{if !$date}
 				{input type="date" label="Date" required=true name="date" default=$now source=$entry}
-				{input type="list" name="user" required=false label="Membre" help="Laisser vide pour une tâche bénévole qui n'est pas liée à un membre en particulier" multiple=false target="!users/selector.php" default=$selected_user can_delete=true}
+				{input type="list" name="users" required=false label=$label help="Laisser vide pour une tâche bénévole qui n'est pas liée à un membre en particulier" multiple=$allow_multiple_users target="!users/selector.php" default=$selected_users can_delete=true}
 			{/if}
 			{input type="select" options=$tasks name="task_id" label="Catégorie" source=$entry default_empty="— Non spécifiée —"}
 			{if $date && $is_today}
